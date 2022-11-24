952412a22e0e7a79f633ec0e0c542dc2190871c1
Pacoalberola <falberola2003@gmail.com>
2022-11-24 17:06:25 +0100

    Commit 4 fin parte 1

--- app/src/main/java/com/example/examenpractico2/VerTotalActivity.kt
+++ app/src/main/java/com/example/examenpractico2/VerTotalActivity.kt
@@ -1,7 +1,9 @@
 package com.example.examenpractico2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class VerTotalActivity : AppCompatActivity() {
@@ -11,6 +13,7 @@ class VerTotalActivity : AppCompatActivity() {
     private lateinit var tvValorBruto: TextView
     private lateinit var tvValorNeto: TextView
     private lateinit var tvValorIVA: TextView
+    private lateinit var btFacturar: Button
     private var resul:Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,16 +23,26 @@ class VerTotalActivity : AppCompatActivity() {
         tvValorBruto = findViewById(R.id.tvValorBruto)
         tvValorIVA = findViewById(R.id.tvValorIVA)
         tvValorNeto = findViewById(R.id.tvValorNeto)
+        btFacturar = findViewById(R.id.btFacturar)
 
         val bruto = intent.getStringExtra(EXTRA_BRUTO)
 
         tvValorBruto.text = bruto
 
-        val num1 = bruto.toString().toInt()
-        resul = ((16 * (num1) / 100) + num1).toDouble()
+        val num1 = bruto.toString().toDouble()
+        resul = (16 * num1 / 100) + num1
 
         tvValorNeto.text = resul.toString()
 
+        btFacturar.setOnClickListener{verFactura()}
 
+
+    }
+
+    fun verFactura(){
+        val intent = Intent()
+        intent.putExtra("Neto", tvValorNeto.text.toString())
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
