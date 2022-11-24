49e03ea652af26919baf553409dc8adbabe40048
Pacoalberola <falberola2003@gmail.com>
2022-11-24 20:13:33 +0100

    Commit 5 fin parte 2

---
+++ app/src/main/java/com/example/examenpractico2/DetallesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.examenpractico2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetallesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalles)
+    }
+}
