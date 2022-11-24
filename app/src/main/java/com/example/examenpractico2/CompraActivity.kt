90433c17a94df165f422676c88776c7e41ab84a9
Pacoalberola <falberola2003@gmail.com>
2022-11-24 16:30:33 +0100

    Commit 2

--- app/src/main/java/com/example/examenpractico2/CompraActivity.kt
+++ app/src/main/java/com/example/examenpractico2/CompraActivity.kt
@@ -4,17 +4,29 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 
 class CompraActivity : AppCompatActivity() {
-
-
 
     private lateinit var etPrecio: EditText
     private lateinit var etCantidad: EditText
     private lateinit var btVerTotal: Button
-    private var resul:Int = 0
+    private lateinit var tvTotal : TextView
+
+    private val verTotalLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ activityResult ->
+        if (activityResult.resultCode == RESULT_OK){
+            val neto = activityResult.data?.getStringExtra("Neto").orEmpty()
+            tvTotal.text = String.format(getString(R.string.total), neto)
+            tvTotal.visibility = View.VISIBLE
+
+        }
+    }
 
 
     @SuppressLint("MissingInflatedId")
@@ -25,20 +37,19 @@ class CompraActivity : AppCompatActivity() {
         etPrecio= findViewById(R.id.etPrecio)
         etCantidad = findViewById(R.id.etCantidad)
         btVerTotal = findViewById(R.id.btVerTotal)
+        tvTotal = findViewById(R.id.tvTotal)
 
-        val num1:Int = Integer.parseInt(etCantidad.text.toString())
-        val num2:Int = Integer.parseInt(etPrecio.text.toString())
-        resul = num1 * num2
 
-        println(resul)
-
+        tvTotal.visibility = View.GONE
         btVerTotal.setOnClickListener{enviarVerTotal()}
-
-
 
     }
 
     fun enviarVerTotal() {
+        val num1:Int = Integer.parseInt(etCantidad.text.toString())
+        val num2:Int = Integer.parseInt(etPrecio.text.toString())
+        val resul = num1 * num2
+
         val intent = Intent(this, VerTotalActivity::class.java)
         intent.putExtra(VerTotalActivity.EXTRA_BRUTO, resul)
         startActivity(intent)
--- app/src/main/java/com/example/examenpractico2/VerTotalActivity.kt
+++ app/src/main/java/com/example/examenpractico2/VerTotalActivity.kt
@@ -2,13 +2,35 @@ package com.example.examenpractico2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class VerTotalActivity : AppCompatActivity() {
     companion object{
-        val EXTRA_BRUTO = "2ExamenPractico.CompraActivity.etPrecio"
+        val EXTRA_BRUTO = "ExamenPractico2.CompraActivity.etPrecio"
     }
+    private lateinit var tvValorBruto: TextView
+    private lateinit var tvValorNeto: TextView
+    private lateinit var tvValorIVA: TextView
+    private var resul:Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ver_total)
+
+        tvValorBruto = findViewById(R.id.tvValorBruto)
+        tvValorIVA = findViewById(R.id.tvValorIVA)
+        tvValorNeto = findViewById(R.id.tvValorNeto)
+
+        val bruto = intent.getStringExtra(EXTRA_BRUTO)
+
+        tvValorBruto.text = String.format(getString(R.string.bruto), bruto)
+
+
+
+
+
+        val num1:Int = Integer.parseInt(EXTRA_BRUTO)
+        resul = (16 * (num1) / 100).toDouble()
+
     }
 }
